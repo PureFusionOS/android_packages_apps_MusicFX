8991839cfdeab6921a0a44e212b5c799a463adf5
Jean-Michel Trivi <jmtrivi@google.com>
2016-07-11 18:00:54 -0700

    Fix UI behavior for virtualizer effect
    Set default virtualizer strength at 0 (effect is created but not
      applied).
    For "is headphone only" behavior: do not use the hardcoded GUID
      of the LVM implementation, query the effect to check whether
      the transaural mode is supported.
    
    Bug 29229872
    
    Change-Id: I2479e9051ccad4dae9e37bf1f30cb38b15ab66cd
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- src/com/android/musicfx/ActivityMusic.java
+++ src/com/android/musicfx/ActivityMusic.java
@@ -208,12 +208,7 @@ public class ActivityMusic extends AppCompatActivity {
 
             if (effect.type.equals(AudioEffect.EFFECT_TYPE_VIRTUALIZER)) {
                 mVirtualizerSupported = true;
-                if (effect.uuid.equals(UUID.fromString("1d4033c0-8557-11df-9f2d-0002a5d5c51b"))
-                    || effect.uuid.equals(UUID.fromString("e6c98a16-22a3-11e2-b87b-f23c91aec05e"))
-                    || effect.uuid.equals(UUID.fromString("d3467faa-acc7-4d34-acaf-0002a5d5c51b"))) {
-                    mVirtualizerIsHeadphoneOnly = true;
-                    Log.d(TAG, effect.name.toString() + ", mVirtualizerIsHeadphoneOnly = " + mVirtualizerIsHeadphoneOnly);
-                }
+                mVirtualizerIsHeadphoneOnly = !isVirtualizerTransauralSupported();
             } else if (effect.type.equals(AudioEffect.EFFECT_TYPE_BASS_BOOST)) {
                 mBassBoostSupported = true;
             } else if (effect.type.equals(AudioEffect.EFFECT_TYPE_EQUALIZER)) {
@@ -858,5 +853,21 @@ public class ActivityMusic extends AppCompatActivity {
             }
         }
         return false;
+    }
+
+    private static boolean isVirtualizerTransauralSupported() {
+        Virtualizer virt = null;
+        boolean transauralSupported = false;
+        try {
+            virt = new Virtualizer(0, android.media.AudioSystem.newAudioSessionId());
+            transauralSupported = virt.canVirtualize(AudioFormat.CHANNEL_OUT_STEREO,
+                    Virtualizer.VIRTUALIZATION_MODE_TRANSAURAL);
+        } catch (Exception e) {
+        } finally {
+            if (virt != null) {
+                virt.release();
+            }
+        }
+        return transauralSupported;
     }
 }
