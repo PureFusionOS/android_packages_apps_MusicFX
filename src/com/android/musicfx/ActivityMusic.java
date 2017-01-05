7bd4a6d78e59ffb34bad78676f75d9c5b2f6191f
maxwen <max.weninger@gmail.com>
2017-01-05 17:30:39 +0100

    MusicFX: fix enablement of virtualizer knob
    Change-Id: If22baa97c90ba23a6172b0970bdf43aa1fe50d64
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- src/com/android/musicfx/ActivityMusic.java
+++ src/com/android/musicfx/ActivityMusic.java
@@ -607,13 +607,13 @@ public class ActivityMusic extends Activity {
      * layouts so they can take over control/events.
      */
     private void updateUIHeadset(boolean force) {
-        boolean enabled = mToggleSwitch.isChecked() && mIsHeadsetOn;
         final Knob bBKnob = (Knob) findViewById(R.id.bBStrengthKnob);
         //bBKnob.setBinary(mIsSpeakerOn);
         bBKnob.setEnabled(mToggleSwitch.isChecked()
                 && (mIsHeadsetOn || mIsSpeakerOn));
         final Knob vIKnob = (Knob) findViewById(R.id.vIStrengthKnob);
-        vIKnob.setEnabled(enabled || !mVirtualizerIsHeadphoneOnly);
+        vIKnob.setEnabled(mToggleSwitch.isChecked()
+                && (mIsHeadsetOn || mIsSpeakerOn));
 
         if (!force) {
             boolean on = ControlPanelEffect.getParameterBoolean(mContext,
@@ -622,7 +622,8 @@ public class ActivityMusic extends Activity {
                     && (mIsHeadsetOn || mIsSpeakerOn) && on);
             on = ControlPanelEffect.getParameterBoolean(mContext,
                     ControlPanelEffect.Key.virt_enabled);
-            vIKnob.setOn((enabled && on) || !mVirtualizerIsHeadphoneOnly);
+            vIKnob.setOn(mToggleSwitch.isChecked()
+                    && (mIsHeadsetOn || mIsSpeakerOn) && on);
         }
     }
 
