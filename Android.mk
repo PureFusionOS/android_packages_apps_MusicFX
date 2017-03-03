a1de727e1e4e0eaabc51e6f0377d64cdb404149e
maxwen <max.weninger@gmail.com>
2017-03-03 02:40:12 +0100

    MusicFX: set proper SDK version
    Change-Id: I897767bc2d84844075bb0e9226531afabc9f34b2
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,13 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
 LOCAL_JAVA_LIBRARIES := framework
-
+LOCAL_MIN_SDK_VERSION := 24
 LOCAL_PACKAGE_NAME := MusicFX
-
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
