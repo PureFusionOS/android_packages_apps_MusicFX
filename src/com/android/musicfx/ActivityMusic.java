bdb51ca820e19b70f0b48924d75c954add30b756
viperusgit <viperusgit@gmail.com>
2017-05-30 01:18:40 -0400

    Improve the overall layout of MusicFX
    MusicFX's stock layout is kind of horrible
    Lets fix that
    
    *Set the in-use header color to primaryColorDark
    *Remove the icon next to the currently in-use audio device
    *Explain why there is two headers by addding in-use to the in-use header
    *Put both spinner titles in bold to make them stand out more
    *Add a spinner disabled color
    
    http://i.imgur.com/I3Oxhhc.jpg
    
    Change-Id: Ib6ab8e94bb9c045e3bf7ecc8bc23edd291d4e231
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- src/com/android/musicfx/ActivityMusic.java
+++ src/com/android/musicfx/ActivityMusic.java
@@ -165,6 +165,7 @@ public class ActivityMusic extends AppCompatActivity {
     private Gallery mGallery;
     private int mHighlightColor;
     private int mTextColor;
+    private int mSpinoffColor;
 
     /**
      * Array containing RSid of preset reverb names.
@@ -243,6 +244,7 @@ public class ActivityMusic extends AppCompatActivity {
         mViewGroup = (ViewGroup) findViewById(R.id.contentSoundEffects);
         mHighlightColor = getResources().getColor(R.color.highlight_gallery_text);
         mTextColor = getResources().getColor(R.color.textColor);
+        mSpinoffColor = getResources().getColor(R.color.spinner_disabled);
 
         // Fill array with presets from AudioEffects call.
         // allocate a space for 1 extra strings (User)
@@ -434,15 +436,13 @@ public class ActivityMusic extends AppCompatActivity {
     }
 
     private void updateCurrentLevelInfo(String level) {
+        String current = getResources().getString(R.string.current_output);
         if (level == ControlPanelEffect.SPEAKER_PREF_SCOPE) {
-            mCurrentLevelText.setText(getResources().getString(R.string.drawer_item_speaker));
-            mCurrentLevelText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_speaker, 0, 0, 0);
+            mCurrentLevelText.setText(current  + " " + getResources().getString(R.string.drawer_item_speaker));
         } else if (level == ControlPanelEffect.HEADSET_PREF_SCOPE) {
-            mCurrentLevelText.setText(getResources().getString(R.string.drawer_item_headset));
-            mCurrentLevelText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_headphones, 0, 0, 0);
+            mCurrentLevelText.setText(current  + " " + getResources().getString(R.string.drawer_item_headset));
         } else if (level == ControlPanelEffect.BLUETOOTH_PREF_SCOPE) {
-            mCurrentLevelText.setText(getResources().getString(R.string.drawer_item_bluetooth));
-            mCurrentLevelText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_bluetooth, 0, 0, 0);
+            mCurrentLevelText.setText(current  + " " + getResources().getString(R.string.drawer_item_bluetooth));
         }
     }
 
@@ -551,7 +551,7 @@ public class ActivityMusic extends AppCompatActivity {
                 if (position == mPRPreset && position != 0 && mPRPresetSpinner.isEnabled()) {
                     ((TextView) item.findViewById(android.R.id.text1)).setTextColor(mHighlightColor);
                 } else {
-                    ((TextView) item.findViewById(android.R.id.text1)).setTextColor(mTextColor);
+                    ((TextView) item.findViewById(android.R.id.text1)).setTextColor(mSpinoffColor);
                 }
                 return item;
             }
@@ -573,7 +573,7 @@ public class ActivityMusic extends AppCompatActivity {
                         if (position != 0 && mPRPresetSpinner.isEnabled()) {
                             tv.setTextColor(mHighlightColor);
                         } else {
-                            tv.setTextColor(mTextColor);
+                            tv.setTextColor(mSpinoffColor);
                         }
                     }
                 }
@@ -596,7 +596,7 @@ public class ActivityMusic extends AppCompatActivity {
                 if (position == mSWStrength && position != 0 && mSWStrengthSpinner.isEnabled()) {
                     ((TextView) item.findViewById(android.R.id.text1)).setTextColor(mHighlightColor);
                 } else {
-                    ((TextView) item.findViewById(android.R.id.text1)).setTextColor(mTextColor);
+                    ((TextView) item.findViewById(android.R.id.text1)).setTextColor(mSpinoffColor);
                 }
                 return item;
             }
@@ -618,7 +618,7 @@ public class ActivityMusic extends AppCompatActivity {
                         if (position != 0 && mSWStrengthSpinner.isEnabled()) {
                             tv.setTextColor(mHighlightColor);
                         } else {
-                            tv.setTextColor(mTextColor);
+                            tv.setTextColor(mSpinoffColor);
                         }
                     }
                 }
